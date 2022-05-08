70d66c7ee4f13a061b05d129c5ba8a0310177ad3
SMetto20 <smetto20@gmail.com>
2022-05-08 14:11:48 +0300

    adds several tests

--- src/main/java/org/example/Endangered.java
+++ src/main/java/org/example/Endangered.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Endangered {
+    private int id;
+    private String name;
+    private int age;
+    private String health;
+
+public int getId(){
+    return id;
+}
+public String getName(){
+    return name;
+}
+
+
 }
